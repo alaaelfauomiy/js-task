3527bd37af8a0d20155ea824cfcdb4e2a2cab34c
alaaelfauomiy <alaamoatzbellah@gmail.com>
2024-07-09 00:38:38 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,44 @@
+function big() {
+    document.getElementById("txt").style.fontSize = "70px";
+}
+
+function small() {
+    document.getElementById("txt").style.fontSize = "20px";
+}
+function changecolor() {
+    document.getElementById("txt").style.color = "green";
+}
+function changeopacity() {
+    document.getElementById("txt").style.opacity = ".5";
+}
+function visibility() {
+    document.getElementById("txt").style.visibility = "hidden";
+}
+function defult() {
+    document.getElementById("txt").style.visibility = "visible";
+    document.getElementById("txt").style.display = "block";
+    document.getElementById("txt").style.fontSize = "2em";
+    document.getElementById("txt").style.opacity = "1";
+    document.getElementById("txt").style.color = "black";
+}
+
+
+
+function border() {
+    document.getElementById("h").style.border = "5px solid pink";
+}
+
+function radius() {
+    document.getElementById("h").style.borderRadius = "20px";
+}
+function visibility2() {
+    document.getElementById("h").style.visibility = "hidden";
+}
+function show() {
+    document.getElementById("h").style.visibility = "visible";
+}
+function changeopacity2() {
+    document.getElementById("h").style.opacity = ".5";
+}
+
+
